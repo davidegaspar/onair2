ea24161c754cb1c3f602bc8c0dd3d33c729af5bb
danielgaspar <danielfantas@hotmail.com>
2019-07-15 23:39:51 +0100

    danielgaspar enviar

--- site/email_sender.php
+++ site/email_sender.php
@@ -35,7 +35,7 @@ $subject = 'NEW MESSAGE FROM YOUR WEBSITE'; // CUSTOMIZE THE SUBJECT OF THE EMAI
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>CONTACTOS</title>
-        <meta name="description" content="Radio station HTML template">
+        <meta name="description" content="FANTAS FM">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <!-- icons -->
