441c09ae02cb74af9bb55c4426b27ad8237e8dd8
danielgaspar <danielfantas@hotmail.com>
2019-09-09 22:58:15 +0100

    danielgaspar enviar

--- site/js/centova/data.js
+++ site/js/centova/data.js
@@ -22,5 +22,32 @@ window.centova.data = {
     "from": ":",
     "to": "",
     "img": "imagestemplate/MUSIC_NONSTOP.jpg"
-  }
-}
+  },
+},
+"2302": {
+  "title": "1H SEM PAUSAS",
+  "link": "daniel-gaspar.html",
+  "host": "COM Daniel Gaspar",
+  "from": "20:00",
+  "to": "21:00",
+  "img": "imagestemplate/IMG_0129.jpg"
+  },
+},
+"2178": {
+  "title": "1H SEM PAUSAS",
+  "link": "davide-gaspar.html",
+  "host": "Com Davide Gaspar",
+  "from": "23:00",
+  "to": "00:00",
+  "img": "imagestemplate/davidegaspar.jpg"
+  },
+
+},
+"97": {
+  "title": "OUT NIGHT",
+  "link": "davide-gaspar.html",
+  "host": "Com Davide e Sara",
+  "from": "21:00",
+  "to": "23:00",
+  "img": "imagestemplate/davidegaspar.jpg"
+},
