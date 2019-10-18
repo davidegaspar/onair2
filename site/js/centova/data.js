982f51920502d4337f87a6d50a6aefbbbaceaed8
danielgaspar <danielfantas@hotmail.com>
2019-10-18 22:10:10 +0100

    danielgaspar enviar

--- site/js/centova/data.js
+++ site/js/centova/data.js
@@ -59,8 +59,8 @@ window.centova.data = {
     "title": "Davide Gaspar",
     "link": "davide-gaspar.html",
     "host": "...",
-    "from": "12:00",
-    "to": "14:00",
+    "from": "22:00",
+    "to": "00:00",
     "img": "imagestemplate/davidegaspar.jpg"
   },
   "2402": {
