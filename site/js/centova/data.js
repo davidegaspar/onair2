bee03be2e936ce69cb6129c893905282480aac38
danielgaspar <danielfantas@hotmail.com>
2019-12-14 01:16:18 +0000

    danielgaspar enviar

--- site/js/centova/data.js
+++ site/js/centova/data.js
@@ -4,7 +4,7 @@ window.centova.data = {
     "link": "musicnonstop.html",
     "host": "COM FANTASFM",
     "from": "00:00",
-    "to": "13:00",
+    "to": "12:00",
     "img": "imagestemplate/MUSIC_NONSTOP.jpg"
   },
   "2398": {
@@ -35,8 +35,8 @@ window.centova.data = {
     "title": "Sara Correia",
     "link": "sara-correia.html",
     "host": "",
-    "from": "17.00",
-    "to": "19:00",
+    "from": "12.00",
+    "to": "14:00",
     "img": "imagestemplate/saracorreia.jpg"
   },
   "2404": {
@@ -120,8 +120,8 @@ window.centova.data = {
     "title": "PILOTO AUTOMATICO",
     "link": "pilotoautomatico.html",
     "host": "Com FANTAS FM",
-    "from": "15:00",
-    "to": "17:00",
+    "from": "16:00",
+    "to": "21:00",
     "img": "imagestemplate/PILOTOAUTOMATICO.jpg"
 
 
