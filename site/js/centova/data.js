11888a0429ec62735e1d48b903ae6d2b4feb0fcc
danielgaspar <danielfantas@hotmail.com>
2019-12-16 23:50:04 +0000

    danielgaspar enviar

--- site/js/centova/data.js
+++ site/js/centova/data.js
@@ -4,7 +4,7 @@ window.centova.data = {
     "link": "musicnonstop.html",
     "host": "COM FANTASFM",
     "from": "00:00",
-    "to": "12:00",
+    "to": "13:00",
     "img": "imagestemplate/MUSIC_NONSTOP.jpg"
   },
   "2398": {
@@ -59,8 +59,8 @@ window.centova.data = {
     "title": "Davide Gaspar",
     "link": "davide-gaspar.html",
     "host": "...",
-    "from": "22:00",
-    "to": "00:00",
+    "from": "19:00",
+    "to": "21:00",
     "img": "imagestemplate/davidegaspar.jpg"
   },
   "2402": {
