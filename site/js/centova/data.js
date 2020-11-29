20ccdd8724a82bd407b8b007f22153867e4823e2
davidegaspar <git@davidegaspar.com>
2020-11-29 15:33:28 +0000

    davidegaspar enviar

--- site/js/centova/data.js
+++ site/js/centova/data.js
@@ -1,161 +1,154 @@
 window.centova.data = {
-  "2397": {
-    "title": "MUSIC NONSTOP",
-    "link": "musicnonstop.html",
-    "host": "COM FANTASFM",
-    "from": "00:00",
-    "to": "13:00",
-    "img": "imagestemplate/MUSIC_NONSTOP.jpg"
+  2397: {
+    title: "MUSIC NONSTOP",
+    link: "musicnonstop.html",
+    host: "COM FANTASFM",
+    from: "00:00",
+    to: "13:00",
+    img: "imagestemplate/MUSIC_NONSTOP.jpg",
   },
-  "2398": {
-    "title": "MEIA HORA SEM PAUSAS",
-    "link": "meiahora-sem-pausas.html",
-    "host": "Com FANTAS FM",
-    "from": "14:00",
-    "to": "15:00",
-    "img": "imagestemplate/MEIAHORASEMPAUSAS.jpg"
+  2398: {
+    title: "MEIA HORA SEM PAUSAS",
+    link: "meiahora-sem-pausas.html",
+    host: "Com FANTAS FM",
+    from: "14:00",
+    to: "15:00",
+    img: "imagestemplate/MEIAHORASEMPAUSAS.jpg",
   },
-  "2401": {
-    "title": "NIGHT SHOW",
-    "link": "night-show.html",
-    "host": "Com Sara Correia",
-    "from": "19:00",
-    "to": "22:00",
-    "img": "imagestemplate/fghghgjhjj.jpg"
+  2401: {
+    title: "NIGHT SHOW",
+    link: "night-show.html",
+    host: "Com Sara Correia",
+    from: "19:00",
+    to: "22:00",
+    img: "imagestemplate/fghghgjhjj.jpg",
   },
-  "2396": {
-    "title": "LUCH TIME",
-    "link": "luchtime.html",
-    "host": "Com Davide Gaspar",
-    "from": "13:00",
-    "to": "15:00",
-    "img": "imagestemplate/luch_time.jpg"
+  2396: {
+    title: "LUCH TIME",
+    link: "luchtime.html",
+    host: "Com Davide Gaspar",
+    from: "13:00",
+    to: "15:00",
+    img: "imagestemplate/luch_time.jpg",
   },
-  "2403": {
-    "title": "1H SEM PAUSAS",
-    "link": "sara-correia.html",
-    "host": "Sara Correia",
-    "from": "21.00",
-    "to": "22:00",
-    "img": "imagestemplate/saracorreia.jpg"
+  2403: {
+    title: "1H SEM PAUSAS",
+    link: "sara-correia.html",
+    host: "Sara Correia",
+    from: "21.00",
+    to: "22:00",
+    img: "imagestemplate/saracorreia.jpg",
   },
-  "2404": {
-    "title": "Nuno Reis",
-    "link": "nunoreis.html",
-    "host": "",
-    "from": "21:00",
-    "to": "22:00",
-    "img": "imagestemplate/nunoreis.jpg"
+  2404: {
+    title: "Nuno Reis",
+    link: "nunoreis.html",
+    host: "",
+    from: "21:00",
+    to: "22:00",
+    img: "imagestemplate/nunoreis.jpg",
   },
-  "2407": {
-    "title": "AFTER SCHOOL",
-    "link": "after-school.html",
-    "host": "Com Daniel Gaspar",
-    "from": "17:00",
-    "to": "19:00",
-    "img": "imagestemplate/AFTERSCHOOL.jpg"
+  2407: {
+    title: "AFTER SCHOOL",
+    link: "after-school.html",
+    host: "Com Daniel Gaspar",
+    from: "17:00",
+    to: "19:00",
+    img: "imagestemplate/AFTERSCHOOL.jpg",
   },
-  "2412": {
-    "title": "Davide Gaspar",
-    "link": "davide-gaspar.html",
-    "host": "...",
-    "from": "16:00",
-    "to": "21:00",
-    "img": "imagestemplate/davidegaspar.jpg"
+  2412: {
+    title: "Davide Gaspar",
+    link: "davide-gaspar.html",
+    host: "...",
+    from: "16:00",
+    to: "21:00",
+    img: "imagestemplate/davidegaspar.jpg",
   },
-  "2402": {
-    "title": "Daniel Gaspar",
-    "link": "daniel-gaspar.html",
-    "host": "",
-    "from": "22:00",
-    "to": "00:00",
-    "img": "imagestemplate/IMG_0129.jpg"
+  2402: {
+    title: "Daniel Gaspar",
+    link: "daniel-gaspar.html",
+    host: "",
+    from: "22:00",
+    to: "00:00",
+    img: "imagestemplate/IMG_0129.jpg",
   },
-  "2415": {
-    "title": "HOUSE PARTY RADIO SHOW",
-    "link": "house-party-radioshow.html",
-    "host": "Com HOUSE PARTY RADIO SHOW",
-    "from": "00:00",
-    "to": "01:00",
-    "img": "imagestemplate/HOUSEPARTYRADIOSHOW.jpg"
+  2415: {
+    title: "HOUSE PARTY RADIO SHOW",
+    link: "house-party-radioshow.html",
+    host: "Com HOUSE PARTY RADIO SHOW",
+    from: "00:00",
+    to: "01:00",
+    img: "imagestemplate/HOUSEPARTYRADIOSHOW.jpg",
   },
-"3757": {
-    "title": "RANDOM",
-    "link": "random.html",
-    "host": "Com FANTAS FM",
-    "from": "15:00",
-    "to": "16:00",
-    "img": "imagestemplate/RANDOM.jpg"
-
+  3757: {
+    title: "RANDOM",
+    link: "random.html",
+    host: "Com FANTAS FM",
+    from: "15:00",
+    to: "16:00",
+    img: "imagestemplate/RANDOM.jpg",
   },
-  "2399": {
-    "title": " 4 ÀS 4",
-    "link": "web9dasemana.html",
-    "host": "Com Daniel Gaspar",
-    "from": "16:00",
-    "to": "17:00",
-    "img": "imagestemplate/.jpg"
-
+  2399: {
+    title: " 4 ÀS 4",
+    link: "web9dasemana.html",
+    host: "Com Daniel Gaspar",
+    from: "16:00",
+    to: "17:00",
+    img: "imagestemplate/.jpg",
   },
-  "2411": {
-    "title": "2H SEM PAUSAS",
-    "link": "2h-sem-pausas.html",
-    "host": "Com FANTAS FM",
-    "from": "14:00",
-    "to": "16:00",
-    "img": "imagestemplate/fantasfm_logo_3.jpg"
-
+  2411: {
+    title: "2H SEM PAUSAS",
+    link: "2h-sem-pausas.html",
+    host: "Com FANTAS FM",
+    from: "14:00",
+    to: "16:00",
+    img: "imagestemplate/fantasfm_logo_3.jpg",
   },
-  "2406": {
-    "title": "HIP HOP MÚSIC",
-    "link": "hip-hop-music.html",
-    "host": "Com Sara Correia",
-    "from": "22:00",
-    "to": "00:00",
-    "img": "imagestemplate/30c1bf283a3730cf16e9e537c3525643.jpg"
-
-
+  2406: {
+    title: "HIP HOP MÚSIC",
+    link: "hip-hop-music.html",
+    host: "Com Sara Correia",
+    from: "22:00",
+    to: "00:00",
+    img: "imagestemplate/30c1bf283a3730cf16e9e537c3525643.jpg",
   },
-  "2413": {
-    "title": "PILOTO AUTOMÁTICO",
-    "link": "pilotoautomatico.html",
-    "host": "Com FANTAS FM",
-    "from": "16:00",
-    "to": "19:00",
-    "img": "imagestemplate/PILOTOAUTOMATICO.jpg"
-
+  2413: {
+    title: "PILOTO AUTOMÁTICO",
+    link: "pilotoautomatico.html",
+    host: "Com FANTAS FM",
+    from: "16:00",
+    to: "19:00",
+    img: "imagestemplate/PILOTOAUTOMATICO.jpg",
   },
-  "2409": {
-    "title": "FEEL THE MUSIC",
-    "link": "feelthemusic.html",
-    "host": "Com FANTAS FM",
-    "from": "00:00",
-    "to": "01:00",
-    "img": "imagestemplate/feelthemusic.jpg"
+  2409: {
+    title: "FEEL THE MUSIC",
+    link: "feelthemusic.html",
+    host: "Com FANTAS FM",
+    from: "00:00",
+    to: "01:00",
+    img: "imagestemplate/feelthemusic.jpg",
   },
-"2417": {
-  "title": "FANTAS SUMMER",
-  "link": "2417",
-  "host": "Com Daniel Gaspar",
-  "from": "17:00",
-  "to": "19:00",
-  "img": "imagestemplate/.jpg"
-},
-"2410": {
-"title": "DESPERTAR",
-"link": "daniel-gaspar.html",
-"host": "Com Daniel Gaspar",
-"from": "11:00",
-"to": "13:00",
-"img": "imagestemplate/aaaabbbbb.jpg"
-
-},
-"3832": {
-"title": "A HORA DO K-POP",
-"link": "a-hora-do-k-pop.html",
-"host": "",
-"from": "19:00",
-"to": "21:00",
-"img": "imagestemplate/.jpg"
-
- };
+  2417: {
+    title: "FANTAS SUMMER",
+    link: "2417",
+    host: "Com Daniel Gaspar",
+    from: "17:00",
+    to: "19:00",
+    img: "imagestemplate/.jpg",
+  },
+  2410: {
+    title: "DESPERTAR",
+    link: "daniel-gaspar.html",
+    host: "Com Daniel Gaspar",
+    from: "11:00",
+    to: "13:00",
+    img: "imagestemplate/aaaabbbbb.jpg",
+  },
+  3832: {
+    title: "A HORA DO K-POP",
+    link: "a-hora-do-k-pop.html",
+    host: "",
+    from: "19:00",
+    to: "21:00",
+    img: "imagestemplate/.jpg",
+  },
+};
