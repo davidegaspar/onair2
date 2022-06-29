77c7effec7bbce8a7f40eb2c7eb2358e54358929
danielgaspar <danielfantas@hotmail.com>
2022-06-29 16:54:43 +0100

    danielgaspar enviar

--- site/js/centova/data.js
+++ site/js/centova/data.js
@@ -4,7 +4,7 @@ window.centova.data = {
     link: "musicnonstop.html",
     host: "COM FANTASFM",
     from: "00:00",
-    to: "11:00",
+    to: "17:00",
     img: "imagestemplate/MUSIC_NONSTOP.jpg",
   },
   2398: {
